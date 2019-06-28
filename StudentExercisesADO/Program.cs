5af331ef98a7dace6383527f5bcba5983aef4bbf
SamBritt <sam.britt2@yahoo.com>
2019-06-28 15:31:12 -0500

    first commit

---
+++ StudentExercisesADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentExercisesADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
